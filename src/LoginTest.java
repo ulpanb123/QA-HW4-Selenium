99f545bcb897c11da0c5a39f993735b034dcc7eb
Ulpan Beken <ulpan.beken@aviasales.ru>
2024-04-14 01:06:25 +0500

    first two parts of the hw

---
+++ src/LoginTest.java
@@ -0,0 +1,86 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+    private WebDriver chromeDriver;
+     private WebDriver firefoxDriver;
+
+    @BeforeClass
+    public void setUp() {
+        ChromeOptions chromeoption = new ChromeOptions();
+        chromeoption.addArguments("--remote-allow-origins=*");
+        System.setProperty("webdriver.chrome.driver", "/opt/homebrew/bin/chromedriver");
+        chromeDriver = new ChromeDriver(chromeoption);
+
+        FirefoxOptions firefoxoption = new FirefoxOptions();
+        firefoxoption.addArguments("--remote-allow-origins=*");
+        System.setProperty("webdriver.gecko.driver", "/opt/homebrew/bin/geckodriver");
+        firefoxDriver = new FirefoxDriver(firefoxoption);
+    }
+
+    @Test
+    public void testLoginWithCorrectCredentials() {
+        String username = "testuser";
+        String password = "testpassword";
+
+        testLogin(chromeDriver, username, password);
+        /*    testLogin(firefoxDriver, username, password);*/
+    }
+
+    private void testLogin(WebDriver driver, String username, String password) {
+        driver.get("file:///Users/ulpanb/IdeaProjects/SeleniumProject/src/loginwebsite/login.html");
+
+        WebElement usernameInput = driver.findElement(By.id("username"));
+        WebElement passwordInput = driver.findElement(By.id("password"));
+        WebElement loginButton = driver.findElement(By.xpath("//input[@type='submit']"));
+
+        usernameInput.sendKeys(username);
+        passwordInput.sendKeys(password);
+        loginButton.click();
+
+        // Check if the welcome page is loaded
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertEquals(currentUrl, "file:///Users/ulpanb/IdeaProjects/SeleniumProject/src/loginwebsite/welcome.html");
+    }
+
+    @Test
+    public void testLoginWithIncorrectCredentials() {
+        String username = "invaliduser";
+        String password = "invalidpassword";
+
+        testInvalidLogin(chromeDriver, username, password);
+          testInvalidLogin(firefoxDriver, username, password);
+    }
+
+    private void testInvalidLogin(WebDriver driver, String username, String password) {
+        driver.get("file:///Users/ulpanb/IdeaProjects/SeleniumProject/src/loginwebsite/login.html");
+
+        WebElement usernameInput = driver.findElement(By.id("username"));
+        WebElement passwordInput = driver.findElement(By.id("password"));
+        WebElement loginButton = driver.findElement(By.xpath("//input[@type='submit']"));
+
+        usernameInput.sendKeys(username);
+        passwordInput.sendKeys(password);
+        loginButton.click();
+
+        // Check if the login page is still loaded
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertNotEquals(currentUrl, "file:///Users/ulpanb/IdeaProjects/SeleniumProject/src/loginwebsite/welcome.html");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        chromeDriver.quit();
+           firefoxDriver.quit();
+    }
+}
+
---
+++ src/PriceComparison.java
@@ -0,0 +1,54 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PriceComparison {
+
+    private static double extractPrice(String text) {
+        String price = text.replaceAll("[^0-9.]", "");
+        return Double.parseDouble(price);
+    }
+
+    private static double getPrice(String url, String xpath) {
+        ChromeOptions chromeoption = new ChromeOptions();
+        chromeoption.addArguments("--remote-allow-origins=*");
+        System.setProperty("webdriver.chrome.driver", "/opt/homebrew/bin/chromedriver");
+        WebDriver driver = new ChromeDriver(chromeoption);
+        driver.get(url);
+        WebElement priceElement = driver.findElement(By.xpath(xpath));
+        double price = extractPrice(priceElement.getText());
+        driver.quit();
+        return price;
+    }
+
+    public static void main(String[] args) {
+        List<String[]> websites = new ArrayList<>();
+        websites.add(new String[]{"iSpace", "https://ispace.kz/iphone/iphone-15/iphone-15-128-gb-chernyy-mtp03hxa", "/html/body/apr-root/apr-product/div/div[1]/div[2]/div[2]/apr-product-card/div/div[1]/div[3]/div/div[1]/div/div/div[2]"});
+        websites.add(new String[]{"Mechta", "https://www.mechta.kz/product/telefon-sotovyy-apple-iphone-15-128gb-black/", "//*[@id=\"q-app\"]/div[1]/div[1]/main/div/div/div/div/div[4]/div[2]/div/div[1]/div[3]/div/div/div[1]/div/div/div[2]"});
+        websites.add(new String[]{"Alser", "https://alser.kz/p/smartfon-apple-iphone-15-256gb-black-mtp63hxa", "//*[@id=\"__nuxt\"]/div/main/div/div/div[2]/div[2]/div[1]/div/div[3]/div[1]/div/div[1]/div/div[2]"});
+
+        List<Double> prices = new ArrayList<>();
+        for (String[] site : websites) {
+            double price = getPrice(site[1], site[2]);
+            prices.add(price);
+            System.out.printf("Price from %s: %.2f Tenge%n", site[0], price);
+        }
+
+        if (!prices.isEmpty()) {
+            double cheapestPrice = prices.stream().min(Double::compare).orElse(0.0);
+            double averagePrice = prices.stream().mapToDouble(Double::doubleValue).average().orElse(0.0);
+            double mostExpensivePrice = prices.stream().max(Double::compare).orElse(0.0);
+
+            System.out.printf("%nCheapest price: %.2f Tenge%n", cheapestPrice);
+            System.out.printf("Average price: %.2f Tenge%n", averagePrice);
+            System.out.printf("Most expensive price: %.2f Tenge%n", mostExpensivePrice);
+        } else {
+            System.out.println("Failed to retrieve prices from all websites.");
+        }
+    }
+}
